25952d458e1eaaac5bcbb9d7e7540782385f2ec2
Ben Greene <BenJGreene@gmail.com>
2022-12-15 00:36:42 +0000

    Update test

--- src/test/kotlin/com/polecatworks/kotlin/k8smicro/HelloTest.kt
+++ src/test/kotlin/com/polecatworks/kotlin/k8smicro/HelloTest.kt
@@ -1,5 +1,6 @@
 package com.polecatworks.kotlin.k8smicro
 
+import com.polecatworks.kotlin.k8smicro.models.Alive
 import com.polecatworks.kotlin.k8smicro.plugins.configureRouting
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -14,16 +15,20 @@ import io.ktor.server.testing.*
 class HelloTest {
 
     @Test
-    fun testEmbeddeded() = testApplication {
+    fun testEmbedded() = testApplication {
         application{
             install(ContentNegotiation) {
                 json()
             }
-            configureRouting()
+            configureRouting(arrayOf(Alive("test1", true)))
         }
         val response = client.get("/")
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals("Hello World!", response.bodyAsText())
+
+        var alive_response = client.get("/health/alive")
+        assertEquals(HttpStatusCode.OK, alive_response.status)
+        assertEquals("{\"id\":1,\"firstName\":\"Ben\",\"lastName\":\"Greene\"}", alive_response.bodyAsText())
     }
 
 
