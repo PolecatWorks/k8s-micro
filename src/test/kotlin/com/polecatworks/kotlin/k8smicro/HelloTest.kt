613281333450bb47a7f6e1da5716157e9d5337eb
Ben Greene <Ben.Greene@smarsh.com>
2022-12-19 13:29:54 +0000

    Add useless test

--- src/test/kotlin/com/polecatworks/kotlin/k8smicro/HelloTest.kt
+++ src/test/kotlin/com/polecatworks/kotlin/k8smicro/HelloTest.kt
@@ -1,6 +1,5 @@
 package com.polecatworks.kotlin.k8smicro
 
-import com.polecatworks.kotlin.k8smicro.plugins.configureHealthRouting
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
@@ -8,28 +7,12 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
-import io.micrometer.prometheus.PrometheusConfig
-import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class HelloTest {
 
     @Test
-    fun testEmbedded() = testApplication {
-        application {
-            install(ContentNegotiation) {
-                json()
-            }
-            val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
-            configureHealthRouting(HealthSystem(), appMicrometerRegistry)
-        }
-        val response = client.get("/")
-        assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("Hello World!", response.bodyAsText())
-
-        var alive_response = client.get("/health/alive")
-        assertEquals(HttpStatusCode.OK, alive_response.status)
-        // assertEquals("{\"id\":1,\"firstName\":\"Ben\",\"lastName\":\"Greene\"}", alive_response.bodyAsText())
+    fun testHello() {
+        println("I am a useless test")
     }
 }
