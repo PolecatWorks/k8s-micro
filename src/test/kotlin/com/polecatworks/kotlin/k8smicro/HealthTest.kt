a4ce7e3f4413fe73352dace28692c8d01494fc26
Ben Greene <BenJGreene@gmail.com>
2022-12-23 10:48:32 +0000

    Add health tests

--- src/test/kotlin/com/polecatworks/kotlin/k8smicro/HealthTest.kt
+++ src/test/kotlin/com/polecatworks/kotlin/k8smicro/HealthTest.kt
@@ -12,8 +12,83 @@ import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotEquals
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
+import kotlin.time.TimeSource.Monotonic.markNow
 
 class HealthTest {
+
+    @Test
+    @OptIn(ExperimentalTime::class)
+    fun testHealthCheck() {
+        // Check valid until margin is met
+        // Check invalid after margin
+        val marginTime = 1.seconds
+
+        val beforeConstruct = markNow()
+        val dut = HealthCheck("dut", marginTime)
+        val afterConstruct = markNow()
+
+        assert(dut.latest > beforeConstruct)
+        assert(afterConstruct > dut.latest)
+
+        val atMarginEnd = dut.latest + marginTime
+
+        val checkAtMarginEnd = dut.check(atMarginEnd)
+        assert(checkAtMarginEnd.valid)
+        val afterMarginEnd = dut.latest + marginTime + 1.seconds
+        val checkAfterMarginEnd = dut.check(afterMarginEnd)
+
+        assertNotEquals(checkAtMarginEnd, checkAfterMarginEnd)
+        assertFalse(checkAfterMarginEnd.valid)
+
+        // Check kick updates latest
+        val oldLatest = dut.latest
+        dut.kick()
+        val newLatest = dut.latest
+        assert(newLatest > oldLatest)
+    }
+
+    @OptIn(ExperimentalTime::class)
+    @Test
+    fun testHealthSystem() {
+        // Check register, deRegister
+        // Check times both in margin, 1 in margin and after removing entry
+        // Check no items in health list
+        val hs = HealthSystem()
+
+        val marginTimeShort = 500.milliseconds
+        val marginTimeLong = 1.seconds
+
+        val myHealthShort = HealthCheck("short", marginTimeShort)
+        val myHealthLong = HealthCheck("long", marginTimeLong)
+
+        hs.registerAlive(myHealthShort)
+        hs.registerAlive(myHealthLong)
+
+        val atShortMarginEnd = myHealthShort.latest + marginTimeShort
+        val checkAtShortMarginEnd = hs.checkAlive(atShortMarginEnd)
+        assert(checkAtShortMarginEnd.valid)
+
+        val afterShortMarginEnd = myHealthShort.latest + marginTimeShort + 1.milliseconds
+        val checkAfterShortMarginEnd = hs.checkAlive(afterShortMarginEnd)
+        assertFalse(checkAfterShortMarginEnd.valid)
+
+        hs.deregisterAlive(myHealthShort)
+        val checkAfterShortMarginEndShortRemoved = hs.checkAlive(afterShortMarginEnd)
+        assert(checkAfterShortMarginEndShortRemoved.valid)
+
+        val afterLongMarginEnd = myHealthLong.latest + marginTimeLong + 1.milliseconds
+        val checkAfterLongMarginEnd = hs.checkAlive(afterLongMarginEnd)
+        assertFalse(checkAfterLongMarginEnd.valid)
+
+        hs.deregisterAlive(myHealthLong)
+        val checkAfterLongMarginEndRemoved = hs.checkAlive(afterLongMarginEnd)
+        assert(checkAfterLongMarginEndRemoved.valid)
+    }
 
     @Test
     fun testEmbedded() = testApplication {
