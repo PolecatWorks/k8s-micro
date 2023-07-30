d2428e7e4bcfd6fce494857ff4472c426da7068f
Ben Greene <BenJGreene@gmail.com>
2023-07-30 19:17:34 +0100

    Avro4k and Confluent Generic

--- src/main/kotlin/com/polecatworks/kotlin/k8smicro/KafkaProcessor.kt
+++ src/main/kotlin/com/polecatworks/kotlin/k8smicro/KafkaProcessor.kt
@@ -1,8 +1,13 @@
 package com.polecatworks.kotlin.k8smicro
 
 import com.github.avrokotlin.avro4k.Avro
+import com.github.avrokotlin.avro4k.AvroName
 import com.github.avrokotlin.avro4k.AvroNamespace
 import com.github.avrokotlin.avro4k.io.AvroEncodeFormat
+import com.github.thake.kafka.avro4k.serializer.Avro4kSerde
+import com.github.thake.kafka.avro4k.serializer.KafkaAvro4kDeserializer
+import com.github.thake.kafka.avro4k.serializer.KafkaAvro4kDeserializerConfig
+import com.github.thake.kafka.avro4k.serializer.KafkaAvro4kSerializer
 import com.polecatworks.kotlin.k8smicro.health.HealthSystem
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde
@@ -13,6 +18,7 @@ import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import mu.KotlinLogging
 import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.common.serialization.Serdes.Long
@@ -52,15 +58,6 @@ pub(crate) struct Chaser {
     pub(crate) previous: Option<i64>,
 }
  */
-@AvroNamespace("com.polecatworks.chaser")
-@Serializable
-data class Chaser(
-    val name: String,
-    val id: String,
-    val sent: Long,
-    val ttl: Long,
-    val previous: Long?
-)
 
 class KafkaProcessor(
     val config: KafkaProcessorConfig,
@@ -68,6 +65,17 @@ class KafkaProcessor(
     val engine: HttpClientEngine,
     val running: AtomicBoolean
 ) {
+
+    @AvroNamespace("com.polecatworks.chaser")
+    @AvroName("Chaser")
+    @Serializable
+    data class Chaser(
+        val name: String,
+        val id: String,
+        val sent: Long,
+        val ttl: Long,
+        val previous: Long? = null
+    )
 
     val schemaRegistryApi = KafkaSchemaRegistryApi(config.schemaRegistry, engine, health, running)
     val chaserSchema = Avro.default.schema(Chaser.serializer())
@@ -81,76 +89,150 @@ class KafkaProcessor(
         while (running.get() && chaserId === null) {
             try {
                 chaserId = schemaRegistryApi.registerSchema("${config.readTopic}-value", chaserSchema.toString())
+                println("Got a chaser = $chaserId")
             } catch (e: Exception) {
                 println("I got the error : $e so sleeping for ${config.querySleep}")
                 delay(config.querySleep)
             }
         }
-        val myChaser = Chaser("ABCNAME", "ID1", 12, 22, previous = null)
-//        val abc = AvroSerializer.serialize(encoder, myChaser)
-        val baos = ByteArrayOutputStream()
-        Avro.default.openOutputStream(Chaser.serializer()) {
-            encodeFormat = AvroEncodeFormat.Binary
-        }.to(baos).write(myChaser).close()
 
-        val baosString = baos.toString()
-        logger.info("Looking at output = $baosString of length ${baosString.length}")
+        if (true) {
+            val myChaser = Chaser("ABCNAME", "ID1", 12, 22, null)
+            val baos = ByteArrayOutputStream()
+            Avro.default.openOutputStream(Chaser.serializer()) {
+                encodeFormat = AvroEncodeFormat.Binary
+            }.to(baos).write(myChaser).close()
 
-//        val ben2 = Chaser.serializer()
-//        logger.info("Serializer = $ben2")
-        val ben = Avro.default.toRecord(Chaser.serializer(), myChaser)
-//        val ben2 = ben.
-        logger.info("Chaser = $ben")
+            val baosString = baos.toString()
+            logger.info("Looking at output = $baosString of length ${baosString.length}")
 
-        logger.info("Chaser Schema registered for topic ${config.readTopic} with id: $chaserId")
+            val ben = Avro.default.toRecord(Chaser.serializer(), myChaser)
+            logger.info("Chaser = $ben")
 
-        val builder = StreamsBuilder()
+            logger.info("Chaser Schema registered for topic ${config.readTopic} with id: $chaserId")
+        }
+        if (true) {
+            val serializer = KafkaAvro4kSerializer()
+            serializer.configure(
+                mapOf(
+                    AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS to "true",
+                    AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryApi.schemaRegistryUrl
+                ),
+                false
+            )
+            val avroSchema = Avro.default.schema(Chaser.serializer())
+            val topic = "input"
+            val subjectName = "$topic-value"
+
+            val myChaser = Chaser("ABCNAME", "ID1", 12, 22)
+            val result = serializer.serialize(topic, myChaser)
+
+            logger.info("Serdes using xxxx = $result")
+        }
+
+        val streamsBuilder = StreamsBuilder()
+        val streamProperties = mapOf<String, String>(
+            StreamsConfig.APPLICATION_ID_CONFIG to "kotlin1",
+            StreamsConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to KafkaAvro4kDeserializer::class.java.name,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvro4kDeserializer::class.java.name,
+            StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG to Serdes.String()::class.java.name,
+//            StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG to Avro4kSerde::class.java.name,
+            StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG to Avro4kSerde::class.java.name,
+            AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryApi.schemaRegistryUrl,
+            KafkaAvro4kDeserializerConfig.RECORD_PACKAGES to KafkaProcessor::class.java.packageName
+        ).toProperties()
+
+        if (false) {
+            // Setup with Avro48 Specific Record
+
+            val mystream0 = streamsBuilder.stream<String, Chaser>(config.readTopic)
+            mystream0
+                .print(Printed.toSysOut<String?, Chaser?>().withLabel("Avro4k"))
+
+            val counts0 = mystream0
+                .map { k, v -> KeyValue("$k-${v.name}", 1L) }
+                .groupByKey(Grouped.with(Serdes.String(), Long()))
+                .reduce { aggValue, newValue -> aggValue!! + newValue!! }
+                .toStream()
+            counts0
+                .print(Printed.toSysOut<String?, Long?>().withLabel("A4k-count"))
+        }
+
+        if (true) {
+            // Setup with Confluent GenericRecord
+            val genericAvroSerde: Serde<GenericRecord> = GenericAvroSerde()
+            genericAvroSerde.configure(
+                mapOf(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryApi.schemaRegistryUrl),
+                false
+            )
+
+            val mystream1 = streamsBuilder
+                .stream(config.readTopic, Consumed.with(Serdes.String(), genericAvroSerde))
+            mystream1
+                .print(Printed.toSysOut<String?, GenericRecord?>().withLabel("Confluent"))
+
+            val counts1 = mystream1
+                .map { k, v ->
+                    val name = v.get("name")
+                    KeyValue("$k-$name", 1L)
+                }
+                .groupByKey(Grouped.with(Serdes.String(), Long()))
+                .reduce { aggValue, newValue -> aggValue!! + newValue!! }
+                .toStream()
+            counts1
+                .print(Printed.toSysOut<String?, Long?>().withLabel("Confluent-count"))
+        }
+        val streams = KafkaStreams(streamsBuilder.build(), streamProperties)
+
+//        val streams1 = KafkaStreams(streamsBuilder.build(), streamProperties)
+
+        val kthread = thread {
+            streams.start()
+        }
 
         logger.info("Reading from input topic ${config.readTopic}")
 
-        val genericAvroSerde: Serde<GenericRecord> = GenericAvroSerde()
-        genericAvroSerde.configure(
-            mapOf(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryApi.schemaRegistryUrl),
-            /*isKey*/ false
-        )
-
-        val records = builder
-            .stream(config.readTopic, Consumed.with(Serdes.String(), genericAvroSerde))
+//            .stream(config.readTopic, Consumed.with(Serdes.String(), Serdes.String()))
         // .print(Printed.toSysOut<String, String>().withLabel("input-stream"))
         // See here for custom serdes: https://github.com/confluentinc/examples/blob/7.3.2-post/clients/cloud/kotlin/src/main/kotlin/io/confluent/examples/clients/cloud/StreamsExample.kt
 
-        val counts = records.map { k, v ->
+//        val counts = records.map { k, v ->
 //            println("OKLY DOKLEY $k, $v")
-            val name = v.get("name").toString()
-//            println("Got id = $name")
-//            AvroSerializer<Chaser>.decodeAvroValue()
+//
+//
+//
+// //            val result = deserializer.deserialize("input-value", v) as Chaser
+// //            println("The output value is ${result.name}")
+//            val name=v.name
+// //            val name = result.name
+//
+// //            val name = v.get("name").toString()
+// //            println("Got id = $name")
+// //            AvroSerializer<Chaser>.decodeAvroValue()
+//
+//            KeyValue(name, 1L)
+// //            KeyValue(k, v.length.toLong())
+//        }
+// //        counts.print(Printed.toSysOut<String, Long>().withLabel("Consumed record"))
+//        val countAgg = counts
+//            .groupByKey(Grouped.with(Serdes.String(), Long()))
+//            .reduce { aggValue, newValue -> aggValue!! + newValue!! }
+//            .toStream()
+//        countAgg.print(Printed.toSysOut<String, Long>().withLabel("Running count"))
 
-            KeyValue(name, 1L)
-//            KeyValue(k, v.length.toLong())
-        }
-//        counts.print(Printed.toSysOut<String, Long>().withLabel("Consumed record"))
-        val countAgg = counts
-            .groupByKey(Grouped.with(Serdes.String(), Long()))
-            .reduce { aggValue, newValue -> aggValue!! + newValue!! }
-            .toStream()
-        countAgg.print(Printed.toSysOut<String, Long>().withLabel("Running count"))
-
-        val streamProperties = mapOf<String, String>(
-            StreamsConfig.APPLICATION_ID_CONFIG to "kotlin1",
-            StreamsConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092"
-        ).toProperties()
-
-        var streams: KafkaStreams? = null
-        val kthread = thread {
-            streams = KafkaStreams(builder.build(), streamProperties)
-//                .use {
-//                it.start()
-// //                println("Starting use")
-//            }
-            logger.info("Constructed Kafka streams")
-            streams!!.start()
-            logger.info("Started Kafka streams")
-        }
+//
+// //        var streams: KafkaStreams? = null
+//        val kthread = thread {
+//            streams = KafkaStreams(builder.build(), streamProperties)
+// //                .use {
+// //                it.start()
+// // //                println("Starting use")
+// //            }
+//            logger.info("Constructed Kafka streams")
+//            streams!!.start()
+//            logger.info("Started Kafka streams")
+//        }
 
         logger.info("Kafka streams started in a a thread")
 
@@ -159,7 +241,7 @@ class KafkaProcessor(
             delay(config.taskSleepDuration)
         }
 
-        streams!!.close()
+        streams.close()
         kthread.join()
         logger.info("Ended kafka task")
     }
