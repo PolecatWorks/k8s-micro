5ab211533ec859fa81d25e5d028cbfe8b6f2d633
Ben Greene <BenJGreene@gmail.com>
2026-01-11 21:50:24 +0000

    Set log leve for health to be debug

--- container/src/main/kotlin/com/polecatworks/kotlin/k8smicro/health/HealthRouting.kt
+++ container/src/main/kotlin/com/polecatworks/kotlin/k8smicro/health/HealthRouting.kt
@@ -1,10 +1,13 @@
 package com.polecatworks.kotlin.k8smicro.health
 
 import com.polecatworks.kotlin.k8smicro.app.AppService
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
@@ -41,7 +44,7 @@ fun Application.configureHealthRouting(
                 val myReady = health.checkReady(now)
                 call.response.status(if (myReady.valid) HttpStatusCode.OK else HttpStatusCode.TooManyRequests)
                 call.application.environment.log
-                    .info("Ready check: $myReady")
+                    .debug("Ready check: $myReady")
                 call.respond(myReady)
 //            call.respondText { "ready" }
             }
@@ -50,7 +53,7 @@ fun Application.configureHealthRouting(
                 val myReady = health.checkAlive(now)
                 call.response.status(if (myReady.valid) HttpStatusCode.OK else HttpStatusCode.NotAcceptable)
                 call.application.environment.log
-                    .info("Alive check: $myReady")
+                    .debug("Alive check: $myReady")
                 call.respond(myReady)
             }
         }
